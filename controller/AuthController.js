bde736a73f1ffea87a7a8df75fc0d62ddd953430
MarkusTech <wm.recopelacion@gmail.com>
2024-07-15 10:58:42 +0800

    Update AuthController.js

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -3,6 +3,7 @@ import vine, { errors } from "@vinejs/vine";
 import { loginSchema, registerSchema } from "../validations/authValidation.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { messages } from "@vinejs/vine/defaults";
 
 class AuthController {
   static async register(req, res) {
@@ -54,18 +55,56 @@ class AuthController {
   static async login(req, res) {
     try {
       const body = req.body;
-
       const validator = vine.compile(loginSchema);
       const payload = await validator.validate(body);
-      return res.json({ payload });
+
+      //   * Find user with email
+      const findUser = await prisma.users.findUnique({
+        where: {
+          email: payload.email,
+        },
+      });
+
+      if (findUser) {
+        if (!bcrypt.compareSync(payload.password, findUser.password)) {
+          return res.status(400).json({
+            errors: {
+              email: "Invalid Credentials.",
+            },
+          });
+        }
+
+        // * Issue token to user
+        const payloadData = {
+          id: findUser.id,
+          name: findUser.name,
+          email: findUser.email,
+          profile: findUser.profile,
+        };
+        const token = jwt.sign(payloadData, process.env.JWT_SECRET, {
+          expiresIn: "365d",
+        });
+
+        return res.json({
+          message: "Logged in",
+          access_token: `Bearer ${token}`,
+        });
+      }
+
+      return res.status(400).json({
+        errors: {
+          email: "No user found with this email.",
+        },
+      });
     } catch (error) {
       console.log("The error is", error);
       if (error instanceof errors.E_VALIDATION_ERROR) {
+        // console.log(error.messages);
         return res.status(400).json({ errors: error.messages });
       } else {
         return res.status(500).json({
           status: 500,
-          message: "Something went wrong. Please try again.",
+          message: "Something went wrong.Please try again.",
         });
       }
     }
